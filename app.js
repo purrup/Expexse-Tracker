82e187b9201509b425c2c6b271f40ad02575edb3
Jerry Tsai <purrup27@gmail.com>
2019-04-28 22:21:52 +0800

    create home router

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use('/', require('./routes/home'))
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
