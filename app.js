e1589fbf2f1f3c72463aec8e0019c30b44ac0547
Jerry Tsai <purrup27@gmail.com>
2019-04-29 00:55:23 +0800

    compelte index wireframe

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 
 app.use('/', require('./routes/home'))
 app.listen(process.env.PORT || 3000, () => {
