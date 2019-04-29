cf6425157725d38ac1be5b9c57db3f94d4546cb5
Jerry Tsai <purrup27@gmail.com>
2019-04-29 21:19:06 +0800

    create new page

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
+// 載入路由
 app.use('/', require('./routes/home'))
+app.use('/records', require('./routes/record'))
+// app.use('/users', require('./routes/user'))
+
+// listen to port
 app.listen(process.env.PORT || 3000, () => {
   console.log('App is running on http://localhost:3000')
 })
---
+++ routes/record.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+// 列出全部 Record
+// router.get('/', (req, res) => {
+//   res.send('display all record')
+// })
+
+//新增一筆 Record 的頁面
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+// 新增一筆 Record
+
+// 修改 Record 頁面
+
+// 修改 Record
+
+module.exports = router
